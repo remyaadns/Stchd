abcefac45565971e35c2763fa07ec9e5e9eac17d
Remya <remyaadns@gmail..com>
2025-08-09 14:31:06 -0400

    Added/Updated Tab Layout with Color Scheme Theme

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,60 +3,73 @@ import React from 'react';
 // import { Ionicons } from '@expo/vector-icons';
 import { Home, Search, Plus, User, Heart } from "lucide-react-native";
 import { useRouter } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
 
 export default function TabLayout() {
   const router = useRouter();
+   const colorScheme = useColorScheme(); 
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#0a7ea4",
+        tabBarActiveTintColor: colorScheme === 'dark' ? '#fff' : '#000',  
+
+       
+        tabBarStyle: {
+          backgroundColor: colorScheme === 'dark' ? '#1f1f1f' : '#fff',  
+          borderTopWidth: 0,
+        },
+        
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen  
-      name="index"
+        name="index"
         options={{
           title: '',
-          tabBarIcon: ({ color, focused }) => 
-            <Home color={color} size={24}  />
+          tabBarIcon: ({ color }) => 
+            <Home color={color} size={24} />
         }}
       />
-            <Tabs.Screen  
-      name="search"
-        options={{
-          title: '',
-          tabBarIcon: ({ color, focused }) => 
-            <Search color={color} size={24} />
-        }} />
       <Tabs.Screen  
-      name="empty"
+        name="search"
         options={{
           title: '',
-          tabBarIcon: ({ color, focused }) => 
+          tabBarIcon: ({ color }) => 
+            <Search color={color} size={24} />
+        }} 
+      />
+      <Tabs.Screen  
+        name="empty"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => 
             <Plus color={color} size={24} />
         }}
         listeners={{
           tabPress: (e) => {
             e.preventDefault();
             router.push('/post');
-          
           },
-        }} />
-             <Tabs.Screen  
-      name="activity"
+        }} 
+      />
+      <Tabs.Screen  
+        name="activity"
         options={{
           title: "",
-          tabBarIcon: ({ color, focused }) => 
+          tabBarIcon: ({ color }) => 
             <Heart color={color} size={24} />
-        }} />
-                     <Tabs.Screen  
-      name="profile"
+        }} 
+      />
+      <Tabs.Screen  
+        name="profile"
         options={{
           title: "",
-          tabBarIcon: ({ color, focused }) => 
+          tabBarIcon: ({ color }) => 
             <User color={color} size={24} />
-        }} />
+        }} 
+      />
     </Tabs>
   );
 }
